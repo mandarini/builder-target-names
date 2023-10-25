89acda0cefbb19981618ddb8f7f3daa6de7df0bd
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-10-25 15:16:07 +0300

    imported in app

--- apps/test/src/app/app.tsx
+++ apps/test/src/app/app.tsx
@@ -1,12 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
+import { Rlrb1 } from '@myorg/rlrb1';
+import { Rlrb2 } from '@myorg/rlrb2';
+import { Rlrswc1 } from '@myorg/rlrswc1';
+import { Rlrswc2 } from '@myorg/rlrswc2';
+import { Rlrv1 } from '@myorg/rlv1';
+import { Rlrv2 } from '@myorg/rlv2';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="test" />
+      <Rlrb1 />
+      <Rlrb2 />
+      <Rlrswc1 />
+      <Rlrswc2 />
+      <Rlrv1 />
+      <Rlrv2 />
     </div>
   );
 }
